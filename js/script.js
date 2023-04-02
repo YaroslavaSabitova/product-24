0e40441fe9c1e36fec54470c8772f6c393c1da81
unknown <slava.sabitova@yandex.ru>
2023-04-02 15:05:28 +0300

    about__right

--- js/script.js
+++ js/script.js
@@ -6,3 +6,37 @@ var rellax = new Rellax('.rellax', {
     vertical: true,
     horizontal: false
 });
+
+const accordion__items = [...document.querySelectorAll('.accordion__item')]; 
+
+accordion__items.forEach((item) => { 
+    const btn = item.querySelector('.accordion__header__title'); 
+    const text = item.querySelector('.accordion__text__wrapper'); 
+    let height; 
+    resize(); 
+
+    if (!item.classList.contains('active')) { 
+        text.style.height = '0px';  
+    }
+
+    function addOrRemoveHeight(height) {
+        if (item.classList.contains('active')) {
+            text.style.height = height + 'px'; 
+        } else {
+            text.style.height = '0px'; 
+        }
+    }
+
+    function resize() {
+        text.style.height = 'auto'; 
+        height = text.scrollHeight; 
+        addOrRemoveHeight(height); 
+    }
+
+    window.addEventListener('resize', resize); 
+
+    btn.addEventListener('click', () => { 
+        item.classList.toggle('active'); 
+        addOrRemoveHeight(height); 
+    });
+});
